43ae442bb5144257824abea75943d65ab0619bd8
Patrick Hirsh <hirshpatrick@gmail.com>
2021-01-23 19:43:02 -0700

    moving to header-only organization model to combat arduino IDE's atrocious multi-file compilation system

--- AmbienceNode/AmbienceNode/AmbienceNode.ino
+++ AmbienceNode/AmbienceNode/AmbienceNode.ino
@@ -1,37 +1,11 @@
-#include <bitswap.h>
-#include <chipsets.h>
-#include <color.h>
-#include <colorpalettes.h>
-#include <colorutils.h>
-#include <controller.h>
-#include <cpp_compat.h>
-#include <dmx.h>
-#include <FastLED.h>
-#include <fastled_config.h>
-#include <fastled_delay.h>
-#include <fastled_progmem.h>
-#include <fastpin.h>
-#include <fastspi.h>
-#include <fastspi_bitbang.h>
-#include <fastspi_dma.h>
-#include <fastspi_nop.h>
-#include <fastspi_ref.h>
-#include <fastspi_types.h>
-#include <hsv2rgb.h>
-#include <led_sysdefs.h>
-#include <lib8tion.h>
-#include <noise.h>
-#include <pixelset.h>
-#include <pixeltypes.h>
-#include <platforms.h>
-#include <power_mgt.h>
-#include <math.h>
+#include "AmbienceNodeCore.h"
 
 #define DATA_PIN 12
 #define NUM_LEDS 10
 #define BAUD_RATE 9600
 
 CRGB leds[NUM_LEDS];
+CHSV dynamicHue = CHSV(0, 255, 255);
 
 void setup() 
 {
@@ -46,17 +20,16 @@ void loop()
 {
   for (int i = 0; i < NUM_LEDS; i++)
   {
-    leds[i] = CRGB(255,0,0);
+    leds[i] = dynamicHue;
   }
-  FastLED.show();
-  delay(500);
-
-  for (int i = 0; i < NUM_LEDS; i++)
+  delay(200);
+  if (dynamicHue.hue == 255)
   {
-    leds[i] = CRGB(0,0,0);
+    dynamicHue.hue = 0;
+  }
+  else
+  {
+    dynamicHue.hue++;
   }
   FastLED.show();
-  delay(500);
-
-  Serial.print("Hello!\n");
 }
---
+++ AmbienceNode/AmbienceNode/AmbienceNodeCore.h
@@ -0,0 +1,38 @@
+#ifndef AMBIENCE_NODE_CORE
+#define AMBIENCE_NODE_CORE
+
+// Standard Library
+#include <math.h>
+
+// ESP Library
+
+// FastLED
+#include <FastLED.h>
+#include <bitswap.h>
+#include <chipsets.h>
+#include <color.h>
+#include <colorpalettes.h>
+#include <colorutils.h>
+#include <controller.h>
+#include <cpp_compat.h>
+#include <dmx.h>
+#include <fastled_config.h>
+#include <fastled_delay.h>
+#include <fastled_progmem.h>
+#include <fastpin.h>
+#include <fastspi.h>
+#include <fastspi_bitbang.h>
+#include <fastspi_dma.h>
+#include <fastspi_nop.h>
+#include <fastspi_ref.h>
+#include <fastspi_types.h>
+#include <hsv2rgb.h>
+#include <led_sysdefs.h>
+#include <lib8tion.h>
+#include <noise.h>
+#include <platforms.h>
+#include <pixelset.h>
+#include <pixeltypes.h>
+#include <power_mgt.h>
+
+#endif
