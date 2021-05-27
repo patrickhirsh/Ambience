715178adb2ba255aac065507afc55cf74edfb695
Patrick Hirsh <patrick@Patricks-Mac-mini.local>
2021-05-26 18:36:21 -0600

    Hello World IOS app

---
+++ AmbienceAppIOS/Ambience/Shared/AmbienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbienceApp.swift
+//  Shared
+//
+//  Created by Patrick Hirsh on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AmbienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
